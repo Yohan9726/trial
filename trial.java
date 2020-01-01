76c7390b13df40a41f6ad4423eb7accbe4e6b7dd
yohan9726 <saptarshig68@gmail.com>
2020-01-01 09:05:40 +0530

    -a

--- trial.java
+++ trial.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 class trial {
     public static void main(String[] args) {
         System.out.println("first file");
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i = 0; i < 10; i++) {
+            a.add(i);
+        }
+        a.forEach(item -> {
+            System.out.print(item + " ");
+        });
     }
 }
