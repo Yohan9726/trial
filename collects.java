228e235e60e5a64970d75808754560383e9dad0d
yohan9726 <saptarshig68@gmail.com>
2020-02-03 09:06:01 +0530

    -a

--- collects.java
+++ collects.java
@@ -21,5 +21,35 @@ class collects {
         a.retainAll(b);
         System.out.println("a.equals(b) = " + a.equals(b));
         System.out.println(a.subList(0,1));
+
+
+        //TreeSet
+
+        TreeSet<Integer> tr = new TreeSet<Integer>();
+
+        tr.add(10);
+        tr.add(10);
+        tr.add(20);
+
+        Iterator<Integer> itr = tr.descendingIterator();
+
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        //Hashmap
+
+        HashMap<String,Integer> hs = new HashMap<String,Integer>();
+
+        hs.put("abc",10);
+        hs.putIfAbsent("abcs",10);
+
+        System.out.println(hs);
+
+        if(hs.containsKey("abc") == true) {
+            hs.replace("abc",20);
+        }
+
+        System.out.println(hs);
     }
 }
