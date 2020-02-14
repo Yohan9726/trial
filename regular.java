9f901a44ea6b2af1ecd193f39c69d0e02ac91fc1
yohan9726 <saptarshig68@gmail.com>
2020-02-14 09:16:28 +0530

    -a

--- regular.java
+++ regular.java
@@ -8,7 +8,7 @@ class regular {
         // [a-z] any values from a to z
         // [A-Z] any values from A to Z
         // writing side by side means OR. E.g. : [a-zA-Z] : a-z or A-Z
-        // * : occurrence of 0 or more times
+        // * : occurrence of 0 or more times (refers to the left-side)
         // \\s : any form of white space
         // [0-9] : Any digits 0 to 9
         // Giving \\s between [a-zA-Z] and [0-9] means it will not accept spaces before [a-zA-Z] or after [0-9]
