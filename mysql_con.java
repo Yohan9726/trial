3e49088cdc0e9de3b49271fec3d8e7445c8c4e08
yohan9726 <saptarshig68@gmail.com>
2020-02-18 11:45:34 +0530

    -a

--- mysql_con.java
+++ mysql_con.java
@@ -6,15 +6,12 @@ class mysql_con {
             Class.forName("com.mysql.cj.jdbc.Driver"); //Using the jdbc driver for mysql
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "saptarshi", "gametweet123");
             Statement st = con.createStatement();
-            //DDL using execute
-            //DML using executeQuery
-            st.execute("DROP TABLE IF EXISTS jdbcTable"); 
-            st.execute("CREATE TABLE jdbcTable (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(20) UNIQUE)");
+            st.executeUpdate("DROP TABLE IF EXISTS jdbcTable"); 
+            st.executeUpdate("CREATE TABLE jdbcTable (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(20) UNIQUE)");
             ResultSet rs = st.executeQuery("SELECT * FROM users2");
             while(rs.next()) {
                 System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
             }
-
             con.close();
         }
 
